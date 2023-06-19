cbf8a70820a194b8af5bbf2b720b18e80c67a8df
WladimirFilho <72136799+WladimirFilho@users.noreply.github.com>
2023-06-19 22:13:54 +1000

    Connection with mongoose, Mong DB and front end. Creating testing request on Postman. Creating first routes.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 import express from "express";
-import products from "./data/products.js";
 // Evirement Variables
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 
 connectDB();
 console.log("Server running");
@@ -26,14 +26,7 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Product from "@/Components/ProductCard";
+import ProductCard from "@/Components/ProductCard";
 import { useState, useEffect } from "react";
 import { fetchProducts } from "@/api/service";
 
@@ -19,7 +19,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold my-8">Latest Products</h1>
       <ul className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-stretch ">
         {products.map((product) => (
-          <Product product={product} />
+          <ProductCard product={product} key={product.id} />
         ))}
       </ul>
     </main>
