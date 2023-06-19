c599298028990b872995f33c422c603f53a9d695
WladimirFilho <72136799+WladimirFilho@users.noreply.github.com>
2023-06-19 22:53:19 +1000

    Custom Errors Middleware

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 
 connectDB();
@@ -27,6 +28,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
